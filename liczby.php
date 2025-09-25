dd6ac1ea930059b8a1a4650e460f1cd153b0cbc7
mateusz <mateuszgalka1234@gmail.com>
2025-09-25 10:36:14 +0200

    lekcja

--- liczby.php
+++ liczby.php
@@ -1,16 +1,9 @@
 <?php
-$liczba1 = 42;
-print ("liczba to:" . $liczba1);
-$liczbaoctalna = 042;
-print("<br>LIczba osemkowa to:" . $liczbaoctalna);
-$LiczbaHex = 2x2A;
-print ("liczba hex:" . $liczbaHex);
-$liczbaduza = 1_234_567;
-print ("liczba  duza to:" . $liczbaduza);
 
-
-
-
-
+$x=0;
+do{
+    echo "The numer is: $x <br>";
+    $x--;
+} while (-10 <= $x)
 
 ?>
